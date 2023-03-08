b76da4d13cda3cc2229eea7a44d7fdca0f88903c
paolocastaldi <pao.gamer97@gmail.com>
2023-03-08 18:06:38 +0100

    Agg. store + struttura componenti + collegamenti

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <div class="container">
+            <div>
+                BOOLFLIX
+            </div>
+
+            <input type="text" placeholder="Cerca per nome">
+        </div>
+    </header>
+</template>
+  
+  
+<script>
+export default {
+
+}
+</script>
+  
+  
+<style lang="scss">
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
